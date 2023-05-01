eff0d7970c9f98109312c4c97ef3789b2ea137c8
Sara Aliaga <sarialiaga5@gmail.com>
2023-05-01 12:58:06 +0000

    Completed Challenge: Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,6 +9,19 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int [] combineArray = new int[arr1.length + arr2.length];
+        int index = 0;
+        for(int i=0; i<arr1.length; i++){
+            combineArray[index]= arr1[i];
+            index++;
+        }
+            for(int i=0; i<arr2.length; i++){
+                combineArray[index]= arr2[i];
+                index++;
+            }
+            
+        
+        return combineArray;
+        
     }
 }
